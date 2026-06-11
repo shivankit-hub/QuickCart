f3062499271cfc6fd7a348476e49d5851bdb5480
shivankit <shivankitagrawal@gmail.com>
2026-06-11 16:40:30 +0530

    Fix MongoDB connection string

--- config/db.js
+++ config/db.js
@@ -14,7 +14,7 @@ async function dbConnect() {
         const opts = {
             bufferCommands: false,
         }
-        cached.promise = mongoose.connect('${process.env.MONGODB_URI}/quickcart', opts).then((mongoose) => {
+        cached.promise = mongoose.connect(process.env.MONGODB_URI, opts).then((mongoose) => {
             return mongoose;
         })
     }
