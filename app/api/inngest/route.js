34404773da936dabed6266afb1523b31efc4f24e
shivankit <shivankitagrawal@gmail.com>
2026-06-11 14:01:28 +0530

    Fix Inngest functions

--- app/api/inngest/route.js
+++ app/api/inngest/route.js
@@ -1,5 +1,10 @@
 import { serve } from "inngest/next";
-import { inngest, syncUserCreation} from "@/config/inngest";
+import {
+  inngest,
+  syncUserCreation,
+  syncUserUpdate,
+  syncUserDeletion
+} from "@/config/inngest";
 
 export const { GET, POST, PUT } = serve({
   client: inngest,
